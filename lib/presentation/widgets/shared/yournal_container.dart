fe0483f0d2d2e87645812a73aa7947d034cd4ce8
Geo290 <jorgeemmanuel.2003@gmail.com>
2024-10-30 09:57:56 -0600

    Feature: container wrapped into a scafold; optional appbar

--- lib/presentation/widgets/shared/yournal_container.dart
+++ lib/presentation/widgets/shared/yournal_container.dart
@@ -2,22 +2,26 @@ import 'package:flutter/material.dart';
 
 class YournalContainer extends StatelessWidget {
   final Widget? child;
-
-  const YournalContainer({super.key, this.child});
+  final PreferredSizeWidget? appBar;
+  const YournalContainer({super.key, this.child, this.appBar});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: RadialGradient(
-          center: Alignment.topLeft,
-          radius: 5.0,
-          stops: <double>[0.0, 0.9],
-          colors:<Color>[Colors.black, Color.fromARGB(46, 12, 12, 12)] 
-        )
-      ),
-      child: Center(
-        child: child,
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: appBar,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: RadialGradient(
+            center: Alignment.topLeft,
+            radius: 5.0,
+            stops: <double>[0.0, 0.9],
+            colors:<Color>[Colors.black, Color.fromARGB(193, 12, 12, 12)]
+          )
+        ),
+        child: Center(
+          child: child,
+        ),
       ),
     );
   }
